5bb180a930421625394a100b48bedf7cef0a9c33
Michael Reda <michaelreda96@gmail.com>
2019-06-03 00:13:06 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BaseUrlInterceptor } from './utils/httpBaseUrlInterceptor';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes} from '@angular/router';
 import {FormsModule} from '@angular/forms'
 import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
 
+
+import { DialogModule } from 'primeng/dialog';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+
+
 import { AppComponent } from './app.component';
 import { RegisterFanIDComponent } from './register-fan-id/register-fan-id.component';
 import { LandingComponent } from './landing/landing.component';
-
 
 
 @NgModule({
@@ -19,6 +27,7 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
@@ -30,7 +39,12 @@ import { LandingComponent } from './landing/landing.component';
          path: 'registerFanId',
          component: RegisterFanIDComponent
       }
-   ])
+   ]),
+   DialogModule,
+   InputTextModule,
+   ButtonModule,
+   MessagesModule,
+   MessageModule
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: BaseUrlInterceptor, multi: true }],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  showLoginDialog: boolean = false;
 
-  constructor() { }
+  username="";
+  password="";
+
+  errors=[];
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  openLoginDialog(){
+    this.showLoginDialog = true;
+  }
+
+  login(){
+    this.errors = [];
+      this.http
+        .post("/login", {
+          username: this.username,
+          password: this.password
+        })
+        .subscribe(res => {
+          console.log(res)
+          if(res["errors"]){
+            this.errors.push({severity:'error', detail:res["errors"][0].msg})
+          }else if(res["msg"]){
+            this.errors.push({severity:'error', detail:res["msg"]});
+          }else if(res["res_status"] && res["res_status"]=="success"){
+            console.log("logged in");
+          }
+        });
   }
 
 }
