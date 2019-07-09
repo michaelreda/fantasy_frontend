f58cde90b87cc568d1dac03d102be06aa320c470
Michael Reda <michaelreda96@gmail.com>
2019-07-09 09:41:29 +0200

    import Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,26 @@
-import { AdminModule } from './admin/admin.module';
-import { HomeModule } from './home/home.module';
-import { LandingModule } from './landing/landing.module';
 import { DefaultLayoutComponent } from './layouts';
 import { RoutesAuthenticationGuardService } from './routes-authentication-guard.service';
 import { RegisterFanIDComponent } from './landing/register-fan-id/register-fan-id.component';
 import { AdminRoutes } from './admin/admin.routes';
 import { LandingLayoutComponent } from './layouts/landing-layout/landing-layout.component';
+import { Routes } from '@angular/router';
 
-export const AppRoutes = [
+export const AppRoutes: Routes = [
       {
         path: '',
         component: LandingLayoutComponent,
-        // loadChildren: 'app/landing/landing.module#LandingModule',
-        loadChidlren: () => LandingModule
+        loadChildren: 'app/landing/landing.module#LandingModule'
       },
       {
         path: 'home',
         component: DefaultLayoutComponent,
         canActivate: [RoutesAuthenticationGuardService],
-        // loadChildren: 'app/home/home.module#HomeModule'
-        loadChidlren: () => HomeModule
+        loadChildren: 'app/home/home.module#HomeModule'
       },
       {
         path: 'admin',
         component: DefaultLayoutComponent,
         canActivate: [RoutesAuthenticationGuardService],
-        // loadChildren: 'app/admin/admin.module#AdminModule'
-        loadChildren: ()=> AdminModule
+        loadChildren: 'app/admin/admin.module#AdminModule'
       }
 ]
