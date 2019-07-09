7b5190212666aec2fc976daba40884b7d7fbf9d8
Michael Reda <michaelreda96@gmail.com>
2019-07-09 07:37:05 +0200

    users service used by admin

--- src/app/admin/services/users.service.ts
+++ src/app/admin/services/users.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,27 @@ import { Injectable } from '@angular/core';
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsersCount(className?:string){
+    let usersCountEndPoint = "/users_count";
+    if(className)
+      usersCountEndPoint += "/"+className;
+    return this.http.get(usersCountEndPoint);
+  }
+
+  getTotalMoneyAndPoints(className?:string){
+    let totalMoneyEndPoint = "/users_total_money_points";
+    if(className)
+      totalMoneyEndPoint += "/"+className;
+    return this.http.get(totalMoneyEndPoint);
+  }
+
+  getUsers(className?:string){
+    let usersPoint = "/users";
+    if(className)
+      usersPoint += "/"+className;
+    return this.http.get(usersPoint);
+  }
+
 }
