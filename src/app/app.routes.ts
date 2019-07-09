84a2c9a7e1ef57bd253bb2ea95553332a3a5f736
Michael Reda <michaelreda96@gmail.com>
2019-07-09 09:35:12 +0200

    routes module ref

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
+import { AdminModule } from './admin/admin.module';
+import { HomeModule } from './home/home.module';
+import { LandingModule } from './landing/landing.module';
 import { DefaultLayoutComponent } from './layouts';
 import { RoutesAuthenticationGuardService } from './routes-authentication-guard.service';
 import { RegisterFanIDComponent } from './landing/register-fan-id/register-fan-id.component';
@@ -8,18 +11,21 @@ export const AppRoutes = [
       {
         path: '',
         component: LandingLayoutComponent,
-        loadChildren: 'app/landing/landing.module#LandingModule',
+        // loadChildren: 'app/landing/landing.module#LandingModule',
+        loadChidlren: () => LandingModule
       },
       {
         path: 'home',
         component: DefaultLayoutComponent,
         canActivate: [RoutesAuthenticationGuardService],
-        loadChildren: 'app/home/home.module#HomeModule'
+        // loadChildren: 'app/home/home.module#HomeModule'
+        loadChidlren: () => HomeModule
       },
       {
         path: 'admin',
         component: DefaultLayoutComponent,
         canActivate: [RoutesAuthenticationGuardService],
-        loadChildren: 'app/admin/admin.module#AdminModule'
+        // loadChildren: 'app/admin/admin.module#AdminModule'
+        loadChildren: ()=> AdminModule
       }
 ]
