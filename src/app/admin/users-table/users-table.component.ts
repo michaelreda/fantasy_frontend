b2365f12b4065e3b789031b2c2142d11c033a645
Michael Reda <michaelreda96@gmail.com>
2019-07-09 07:37:20 +0200

    users table component

--- src/app/admin/users-table/users-table.component.ts
+++ src/app/admin/users-table/users-table.component.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-table.component.css']
 })
 export class UsersTableComponent implements OnInit {
+  users$;
+  cols = [
+    { field: 'name', header: 'Name' },
+    { field: 'username', header: 'Username' },
+    { field: 'teamName', header: 'teamName' },
+    { field: 'class', header: 'Class' },
+    { field: 'points', header: 'Points' },
+    { field: 'money', header: 'Money' },
+  ]
+  constructor(private usersService: UsersService) { 
+    this.users$ = this.usersService.getUsers();
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
