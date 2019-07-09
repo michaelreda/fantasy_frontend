4a7da16b0385b9eed142279f09c55550dfaebcf0
Michael Reda <michaelreda96@gmail.com>
2019-07-09 09:20:47 +0200

    routes abs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,18 +8,18 @@ export const AppRoutes = [
       {
         path: '',
         component: LandingLayoutComponent,
-        loadChildren: 'src/app/landing/landing.module#LandingModule',
+        loadChildren: 'app/landing/landing.module#LandingModule',
       },
       {
         path: 'home',
         component: DefaultLayoutComponent,
         canActivate: [RoutesAuthenticationGuardService],
-        loadChildren: 'src/app/home/home.module#HomeModule'
+        loadChildren: 'app/home/home.module#HomeModule'
       },
       {
         path: 'admin',
         component: DefaultLayoutComponent,
         canActivate: [RoutesAuthenticationGuardService],
-        loadChildren: 'src/app/admin/admin.module#AdminModule'
+        loadChildren: 'app/admin/admin.module#AdminModule'
       }
 ]
