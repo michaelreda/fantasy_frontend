88d42e6e17b6a9cefb5e945a6a97d4aa293ffe9c
Michael Reda <michaelreda96@gmail.com>
2019-07-11 23:23:10 +0200

    dynamic base url

--- src/app/utils/interceptors/httpBaseUrlInterceptor.ts
+++ src/app/utils/interceptors/httpBaseUrlInterceptor.ts
@@ -5,15 +5,16 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
-  baseUrl = "https://rfc-fantasy-server.herokuapp.com"
+  baseUrl = "https://rfc-fantasy-mirror.herokuapp.com"
   constructor(){
+    this.baseUrl = window.location.origin;
     if(window.location.origin.includes("localhost"))
       this.baseUrl = "http://localhost:8080";
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
     req = req.clone({
-      url: this.baseUrl + req.url
+      url: this.baseUrl + '/api' + req.url
     });
     return next.handle(req);
   }
